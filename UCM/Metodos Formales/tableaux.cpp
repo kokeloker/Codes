472eafd1aa8e3fabfd18be9331a90484a4deb7af
root <root@localhost>
2014-12-18 22:05:16 -0300

    delta rules added

--- UCM/Metodos Formales/tableaux.cpp
+++ UCM/Metodos Formales/tableaux.cpp
@@ -15,7 +15,9 @@ string beta_reglas[3][5] = {
 	"A"		,"~A"		,"~A",		"A,B",		"A,~B",
 	"B"		,"~B"		,"B",		"~A,~B",	"~A,B"
 };
+string delta_reglas[3][2] = {
 
+};
 vector <string> getPremisas(string data){
 	int pos = 0;
 	int i;
